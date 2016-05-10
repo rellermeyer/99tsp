b551fe44ef762577f4c68d9a13e81394e2134c64
Kshitij Delvadiya <kshitij.delvadiya@utexas.edu>
2016-05-09 23:13:53 -0500

    Update Rules.mk for Rust

--- rust/Rules.mk
+++ rust/Rules.mk
@@ -2,6 +2,5 @@ CC:=rustc
 # In order to allow multiple extensions
 # Effectively ovveride RUNCMD from Rules.mk in root.
 # The run flags and testfile will be supplied twice but that doesn't matter
-RUNCMD:=./2opt $(RUNFLAGS) $(TEST_FILE)$(EXTENSION)
 
 include ../../Rules.mk
