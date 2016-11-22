293eabc1b691e6e46f407fd6f6222d1900828d75
Loc Hoang <l_hoang@utexas.edu>
2016-11-21 23:22:16 -0600

    comments for top level Rules and java rules/dummy

--- Rules.mk
+++ Rules.mk
@@ -1,6 +1,9 @@
+# Compiles the source file using the specified compiler + flags
+# Note $< is the first specified dependency, in this case the source
 compile: $(SRC)
 	$(CC) $(CFLAGS) $<
 
+# Runs the code.
 run: compile
 	$(RUNCMD) $(RUNFLAGS) $(TEST_FILE)
 
--- java/Rules.mk
+++ java/Rules.mk
@@ -1,7 +1,10 @@
+# Specify how to compile + how to run
 CC:=javac
 CFLAGS:=
 OBJ:=$(SRC:.java=.class)
 RUNCMD:=java
 RUNFLAGS:=-cp . $(OBJ:.class=)
 
+# include the top level Rules.mk (note this is called from the context
+# of the Makefile in a directory below this one)
 include ../../Rules.mk
