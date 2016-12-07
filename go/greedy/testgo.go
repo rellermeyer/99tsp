7edffbc04f393e2e2025c5c953b06f4546c0555f
Chuqi Zhou <cz4792@utexas.edu>
2016-12-07 17:50:32 -0600

    add comments

--- go/greedy/testgo.go
+++ go/greedy/testgo.go
@@ -10,7 +10,7 @@ import (
     "math/rand"
 )
 
-// Run the program
+// Run the program.
 func main() {
     fmt.Println("99 Traveling Salespeople Problem - go version")
     parseInput()
@@ -21,22 +21,22 @@ func parseInput() {
 
     // Open the input file.
     // f, _ := os.Open("input/rat99.tsp")
-    // f, _ := os.Open("input/a280.tsp")
+    f, _ := os.Open("input/a280.tsp")
     // f, _ := os.Open("input/pla85900.tsp")
-    f, _ := os.Open("input/burma14.tsp")
+    // f, _ := os.Open("input/burma14.tsp")
     defer f.Close()
 
-    // Use a scanner to read lines from the input file
+    // Use a scanner to read lines from the input file.
     scanner := bufio.NewScanner(f)
     arraySize := 0
     j := 0
 
-    // Loop over all lines in the file and print them.
+    // Skip first few lines and get the number of dimension.
     for (scanner.Scan()) {
         line := scanner.Text()
         str := strings.Fields(line)
 
-        // Get the number of dimension
+        // Get the number of dimension.
         if (strings.Contains(str[0], "DIMENSION")) {
             for i := 1; i < len(str); i++ {
                 nextToken, _ := strconv.Atoi(str[i])
@@ -47,32 +47,38 @@ func parseInput() {
             break
         }
     }
-    a := make([]float64, arraySize + 1)
-    b := make([]float64, arraySize + 1)
+
+    // Create two arrays to store x and y coordinates respectively.
+    x := make([]float64, arraySize + 1)
+    y := make([]float64, arraySize + 1)
+
+    // Read all x, y coordinates of all points.
     for (scanner.Scan()) {
         line := scanner.Text()
         str := strings.Fields(line)
         firstToken, _ := strconv.Atoi(str[0])
         if (firstToken != 0 && line != "EOF") {
             j++
-            a[j], _ = strconv.ParseFloat(str[1], 64)
-            b[j], _ = strconv.ParseFloat(str[2], 64)
+            x[j], _ = strconv.ParseFloat(str[1], 64)
+            y[j], _ = strconv.ParseFloat(str[2], 64)
         }
         if (str[0] == "EOF") {
             break
         }
     }
-    findRoute(a, b, arraySize)
+    findRoute(x, y, arraySize)
     fmt.Println(arraySize)
 }
 
 // Find the closest point to this point.
-func findNearNeighbor(a []float64, b []float64, points map[int]int, start int) int {
+func findNearNeighbor(x []float64, y []float64, points map[int]int, start int) int {
     neighbor := 0
     smDist := 10000000.0
+
+    // Loop through all yet visited points to find out the cloesest point to current point.
     for i := 1; i < len(points); i++ {
         if (i != start) && (points[i] != 0) {
-            sqSum := math.Pow(a[i] - a[start], 2) + math.Pow(b[i] - b[start], 2)
+            sqSum := math.Pow(x[i] - x[start], 2) + math.Pow(y[i] - y[start], 2)
             dist := math.Sqrt(sqSum)
             if dist < smDist {
                 neighbor = i
@@ -80,29 +86,29 @@ func findNearNeighbor(a []float64, b []float64, points map[int]int, start int) i
             }
         }
     }
-    // fmt.Println(start)
-    // fmt.Println(a[start], b[start])
-    // fmt.Println(neighbor)
-    // fmt.Println(a[neighbor], b[neighbor])
     return neighbor
 }
 
-// Use greedy algorithm to find the solution route
-func findRoute(a []float64, b []float64, arraySize int) {
+// Use greedy algorithm to find the solution route.
+func findRoute(x []float64, y []float64, arraySize int) {
     
-    // Randomly generate a start point
+    // Randomly generate a start point.
     startpoint := rand.Intn(arraySize) + 1
+
+    // Create an array to save the order of points to visit as the solution.
     solutionRoute := make([]int, arraySize + 1)
     solutionRoute[1] = startpoint
 
+    // Create a map to store the points which have not been visited yet.
     unvisitedPoints := make(map[int]int)
     for l := 0; l <= arraySize; l++ {
         unvisitedPoints[l] = l
     }
     unvisitedPoints[startpoint] = 0
 
+    // Find the cloesest point to the current one and go to that point, and repeat.
     for k := 2; k <= arraySize; k++ {
-        nearNeighbor := findNearNeighbor(a, b, unvisitedPoints, startpoint)
+        nearNeighbor := findNearNeighbor(x, y, unvisitedPoints, startpoint)
         solutionRoute[k] = nearNeighbor
         startpoint = nearNeighbor
         unvisitedPoints[nearNeighbor] = 0
@@ -116,9 +122,4 @@ func printResult(solutionRoute []int) {
         fmt.Print(solutionRoute[z], "-")
     }
     fmt.Println(solutionRoute[len(solutionRoute) - 1])
-    // for z := 0; z<len(unvisitedPoints); z++ {
-    //     if (unvisitedPoints[z] != 0) {
-    //         fmt.Println("WHAT",z)
-    //     }
-    // }
 }
